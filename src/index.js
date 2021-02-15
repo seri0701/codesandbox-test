bf4b9a553b0db99ab212e8a68c1bf2a859ebde86
seri0701 <76086657+seri0701@users.noreply.github.com>
2021-02-16 06:47:57 +0900

    2/16 2/16 分割代入

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import "./styles.css";
+// //アロー関数省略
+// const func21 = (str) => str;
+// console.log(func21("func21です"));
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const func3 = (num1, num2) => num1 - num2;
+// console.log(func3(10, 20));
+
+//分割代入
+// const myProfile = {
+//   name: "せり",
+//   age: 38
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です`;
+// console.log(message2);
+
+const myProfile = ["せり", 38];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
