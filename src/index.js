6bbe1bb46832635b92deea90237d233daf14083a
seri0701 <76086657+seri0701@users.noreply.github.com>
2021-02-21 07:42:10 +0900

    2/21 2/21

--- src/index.js
+++ src/index.js
@@ -66,9 +66,31 @@
 
 //mapとfilter
 const nameArr = ["田中", "山田", "じゃけぇ"];
-// nameArr.map((name) => console.log(name));
-const numArr = [1, 2, 3, 4, 5];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 1;
-});
-console.log(newNumArr);
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index}番目は ${nameArr[index]}です`);
+// }
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+// 三項演算子
+// //ある条件　？　条件がtrueの時　：　条件がfalseの時
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+const num = 1300;
+console.log(num.toLocaleString());
