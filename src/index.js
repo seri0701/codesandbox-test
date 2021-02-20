dbf1842d2f4daabe990bfb655cd9d79063fe7493
seri0701 <76086657+seri0701@users.noreply.github.com>
2021-02-20 20:57:48 +0900

    2/20 2/20

--- src/index.js
+++ src/index.js
@@ -40,3 +40,35 @@
 // const sumFunc = (num1, num2) => console.log(num1 + num2);
 // sumFunc(arr1[0], arr1[1]);
 // sumFunc(...arr1);
+
+//まとめる
+// const arr2=[1,2,3,4,5];
+// const [num1, num2, ...arr3]= arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5]
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr4);
+
+//mapとfilter
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// nameArr.map((name) => console.log(name));
+const numArr = [1, 2, 3, 4, 5];
+const newNumArr = numArr.filter((num) => {
+  return num % 2 === 1;
+});
+console.log(newNumArr);
