d7b1e98ed42467ab1c11ddb2b65eef042f773cbf
seri0701 <76086657+seri0701@users.noreply.github.com>
2021-02-22 06:38:44 +0900

    2/22完結 2/22完結

--- src/index.js
+++ src/index.js
@@ -92,5 +92,36 @@ const nameArr = ["田中", "山田", "じゃけぇ"];
 // const val1 = 1 < 0 ? "trueです" : "falseです";
 // console.log(val1);
 
-const num = 1300;
-console.log(num.toLocaleString());
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "１００を超えています！" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 60));
+
+//論理演算子の本当の意味を知ろう　&&  ||
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("１か２はtrueになります");
+// }
+
+// if (flag1 && flag2) {
+//   console.log("１か２はtrueになります");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+//　&&は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
